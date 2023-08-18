30008c1c2f82ad76e9254a0b8ffcad7bf828d059
abdalla morsy <abdallahmorsy@hotmail.com>
2023-08-17 19:25:01 -0700

    0x10

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,11 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
 #include <stdarg.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-
-#endif /* VARIADIC_FUNCTIONS_H */
-
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif
